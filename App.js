f10ffa3ee5a1b2c7404004bd0d6ce63791226799
vibin17604 <108916451+vibin17604@users.noreply.github.com>
2023-03-29 10:47:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Car from './Challenge Yourself/Car';
+import Location from './Challenge Yourself/Day 2/Location';
+
+
+function App() {
+  return (
+    <div>
+       <Car/>
+       <Location/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Location.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Location() {
+  return (
+    <div>
+        <p>Company Name:Audi</p>
+        <h>City Name:Coimbatore</h>
+        </div>
+    
+  );
+}
+
+export default Location
